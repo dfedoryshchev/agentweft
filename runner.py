eb248980ca3b76015ff66a5fd0021737a60fea56
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2025-09-11 20:14:27 +0100

    journal was recording the wrong flow name

--- runner.py
+++ runner.py
@@ -207,7 +207,7 @@ def main():
 
     journal = Path("runs") / "journal.md"
     f = open(journal, "a")
-    f.write(started.strftime("%Y-%m-%d %H:%M") + "  " + flow + "  ok  "
+    f.write(started.strftime("%Y-%m-%d %H:%M") + "  " + config(flow)["name"] + "  ok  "
             + str(int((datetime.datetime.now() - started).total_seconds())) + "s\n")
     f.close()
 
