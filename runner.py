f8cc1fb2d5bedd14c66678188c53cdd700b01b6c
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2025-10-07 20:03:28 +0100

    wip: pull the prompt loading out of runner.py

--- runner.py
+++ runner.py
@@ -27,6 +27,15 @@ def load_env():
         os.environ[k] = v
 
 
+def load_prompt(flow, name, rules):
+    # pulling this out of main so main stops being the only place that knows
+    # how a prompt gets built. half done.
+    text = read(flow, name) + "\n\n" + rules
+    for key in ["INBOX", "LOGS", "WATCH"]:
+        text = text.replace("{" + key + "}", os.environ.get(key, ""))
+    return text
+
+
 def read(flow, name):
     f = open(flow_path(flow, name))
     text = f.read()
