90e47de83a4bbb5e697654c0202e6d56a58eb00d
Dmitriy Fedoryshchev <dmitriy.fedoryshchev@gmail.com>
2025-11-19 20:28:03 +0000

    mark a run as resumed

--- runner/engine.py
+++ runner/engine.py
@@ -229,7 +229,8 @@ def main():
 
     journal = Path("runs") / "journal.md"
     f = open(journal, "a")
-    f.write(started.strftime("%Y-%m-%d %H:%M") + "  " + config(flow)["name"] + "  ok  "
+    f.write(started.strftime("%Y-%m-%d %H:%M") + "  " + config(flow)["name"]
+            + ("  ok (resumed)  " if pick_up else "  ok  ")
             + str(int((datetime.datetime.now() - started).total_seconds())) + "s  "
             + path.name + "\n")
     f.close()
